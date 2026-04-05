52c2a180c31e5202111268cc5601f090df4d5f1c
QUDDUS <qudduslarek@proton.me>
2026-04-05 15:38:15 +0500

    make more moduler code

--- components/custom/header.tsx
+++ components/custom/header.tsx
@@ -1,87 +1,17 @@
-import { Button, Input, SelectTrigger, SelectValue, Tabs } from "@heroui/react";
-import { Label, ListBox, Select } from "@heroui/react";
+import { Button } from "@heroui/react";
 import { ToggleThemeButton } from "./toggle-theme";
-import { PlayIcon, SquareSplitHorizontalIcon } from "@phosphor-icons/react";
-import { useViewer } from "@/stores/tabs.state";
-
-function TabsView() {
-  const { setViewerState } = useViewer();
-  return (
-    <Tabs className="text-muted">
-      <Tabs.ListContainer>
-        <Tabs.List aria-label="Options">
-          <Tabs.Tab
-            id="code"
-            onClick={() => setViewerState({ viewerState: "code" })}
-          >
-            Code
-            <Tabs.Indicator />
-          </Tabs.Tab>
-          <Tabs.Tab
-            id="output"
-            onClick={() => setViewerState({ viewerState: "output" })}
-          >
-            Output
-            <Tabs.Indicator />
-          </Tabs.Tab>
-          <Tabs.Tab
-            id="both"
-            onClick={() => setViewerState({ viewerState: "both" })}
-          >
-            <Tabs.Separator />
-            <SquareSplitHorizontalIcon size={20} weight="duotone" />
-            Split
-            <Tabs.Indicator />
-          </Tabs.Tab>
-        </Tabs.List>
-      </Tabs.ListContainer>
-    </Tabs>
-  );
-}
-
-const languages = [
-  { id: "typescript", label: "TypeScript" },
-  { id: "javascript", label: "JavaScript" },
-  { id: "python", label: "Python" },
-  { id: "java", label: "Java" },
-  { id: "rust", label: "Rust" },
-  { id: "go", label: "Go" },
-  { id: "php", label: "PHP" },
-  { id: "csharp", label: "C#" },
-  { id: "swift", label: "Swift" },
-  { id: "kotlin", label: "Kotlin" },
-];
-
-export function SelectLanguage() {
-  return (
-    <Select className={"w-[124px]"} placeholder="Select language" variant="secondary">
-      <Select.Trigger>
-        <Select.Value />
-        <Select.Indicator />
-      </Select.Trigger>
-      <Select.Popover className={"p-1 rounded-2xl"}>
-        <Input type="text" placeholder="e.g swift" variant="secondary" />
-        <ListBox>
-          {languages.map(({ id, label }) => (
-            <ListBox.Item key={id} id={id} textValue={label} aria-label={label}>
-              {label}
-              <ListBox.ItemIndicator />
-            </ListBox.Item>
-          ))}
-        </ListBox>
-      </Select.Popover>
-    </Select>
-  );
-}
+import { PlayIcon } from "@phosphor-icons/react";
+import { CodeLanguageSelector } from "./selector.minor";
+import { TabsView } from "./tabs.minor";
 
 export function Header() {
   return (
     <div className="w-full py-2 px-4 flex items-center justify-between font-mono">
       <p className="text-sm uppercase tracking-[0.2em]">ONE PLAYGROUND</p>
       <div className="flex gap-2 items-center">
-        <SelectLanguage />
+        <CodeLanguageSelector />
         <TabsView />
-        <Button isIconOnly>
+        <Button isIconOnly variant="primary">
           <PlayIcon weight="fill" />
         </Button>
       </div>
