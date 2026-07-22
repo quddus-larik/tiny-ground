eb9eddfd5f18102c7671b0636dbd34c460a92224
QUDDUS <qudduslarek@proton.me>
2026-07-22 23:58:51 +0500

    chore: add to code route the main emotei

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import { Header } from "@/components/custom/header";
-import { ViewerLayout } from "@/components/layouts/tabs.layout";
-import { TinyIcon } from "@/components/assets/logo";
+import {Header} from "@/components/custom/header";
+import {ViewerLayout} from "@/components/layouts/tabs.layout";
+import {TinyIcon} from "@/components/assets/logo";
 
 export default function Home() {
-  return (
-    <div className="h-svh w-full  relative">
-        <TinyIcon />
-      <Header />
-      <ViewerLayout />
-    </div>
-  );
+    return (
+        <>
+            <TinyIcon className={"absolute bottom-0 right-5 z-10 size-32"}/>
+            <div className="h-svh w-full">
+                <Header/>
+                <ViewerLayout/>
+            </div>
+        </>
+    );
 }
